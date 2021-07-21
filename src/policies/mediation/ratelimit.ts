6467af5853b8ce615b9bc90bb001caaf3de71c63
Robisson Oliveira <robisson@robissonoliveira.com.br>
2021-07-21 20:09:54 -0300

    Optimizing rate_limit

--- src/policies/mediation/ratelimit.ts
+++ src/policies/mediation/ratelimit.ts
@@ -2,23 +2,15 @@ import * as rateLimit from "express-rate-limit";
 import { Application } from "express"
 
 const setupRateLimit = (app: Application, route: any) => {
-    route.product_request_policices.forEach((policie: any) => {
-        if (policie.type === "rate_limit") {
-            app.use(route.api_path, rateLimit(policie.properties));
-        }
-    })
-    
-    route.api_request_policices.forEach((policie: any) => {
-        if (policie.type === "rate_limit") {
-            app.use(route.api_path, rateLimit(policie.properties));
-        }
-    })
-    
-    route.resource_request_policices.forEach((policie: any) => {
-        if (policie.type === "rate_limit") {
-            app.use(route.api_path, rateLimit(policie.properties));
-        }
-    })
+    [
+        "product_request_policices", "api_request_policices", "resource_request_policices"
+    ].forEach((policie: any) => {
+        route[policie].forEach((policie: any) => {
+            if (policie.type === "rate_limit") {
+                app.use(route.api_path, rateLimit(policie.properties));
+            }
+        })
+    });
 }
 
 export default setupRateLimit;
